dd2a50f8c3dc53fec9dce1cbb335c5759c832529
pl3lee <billy.lee@coalitioninc.com>
2025-04-21 18:30:18 -0400

    trying to get devcontainers to work...

--- .config/nvim/lua/config/plugins/devcontainer.lua
+++ .config/nvim/lua/config/plugins/devcontainer.lua
@@ -1,12 +1,6 @@
 return {
     {
-        "amitds1997/remote-nvim.nvim",
-        version = "*",                       -- Pin to GitHub releases
-        dependencies = {
-            "nvim-lua/plenary.nvim",         -- For standard functions
-            "MunifTanjim/nui.nvim",          -- To build the plugin UI
-            "nvim-telescope/telescope.nvim", -- For picking b/w different remote methods
-        },
-        config = true,
+        'https://codeberg.org/esensar/nvim-dev-container',
+        config = function() require("devcontainer").setup {} end
     }
 }
