d206fc11dfd4951bf0c1cd30e58255a44f682bf7
pl3lee <billy.lee@coalitioninc.com>
2025-04-30 10:32:24 -0400

    disabled format on save

--- .config/nvim/lua/config/plugins/lsp.lua
+++ .config/nvim/lua/config/plugins/lsp.lua
@@ -115,14 +115,23 @@ return {
         'stevearc/conform.nvim',
         event = { 'BufWritePre' },
         cmd = { 'ConformInfo' },
+        keys = {
+            {
+                '<leader>f',
+                function() require('conform').format({async = true, lsp_format = 'fallback'}) end,
+                mode = '',
+                desc = '[F]ormat buffer',
+            }
+        },
         opts = {
             notify_on_error = false,
-            format_on_save = function(bufnr)
-                return {
-                    timeout_ms = 5000,
-                    lsp_format = 'fallback',
-                }
-            end,
+            -- format_on_save = function(bufnr)
+            --     if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then return end
+            --     return {
+            --         timeout_ms = 5000,
+            --         lsp_format = 'fallback',
+            --     }
+            -- end,
             formatters_by_ft = {
                 lua = { 'stylua' },
                 python = { 'isort', 'black' },
