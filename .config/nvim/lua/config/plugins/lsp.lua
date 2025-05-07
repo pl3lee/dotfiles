de8128a4d5f02c0e8bcd08a9a392197a6ad80492
pl3lee <billy.lee@coalitioninc.com>
2025-05-07 14:32:26 -0400

    python hanging indent

--- .config/nvim/lua/config/plugins/lsp.lua
+++ .config/nvim/lua/config/plugins/lsp.lua
@@ -66,7 +66,6 @@ return {
                         plugins = {
                             pycodestyle = {
                                 maxLineLength = 100,
-                                hangClosing = true,
                             }
                         }
                     }
