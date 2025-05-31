aebadaaa530da01c2f3174b1977369b934417dfc
pl3lee <billy.lee@coalitioninc.com>
2025-05-31 15:58:57 -0400

    removed harpoon and marks for arrow

---
+++ .config/nvim/lua/config/plugins/arrow.lua
@@ -0,0 +1,9 @@
+return {
+    "otavioschwanck/arrow.nvim",
+    opts = {
+        show_icons = true,
+        separate_by_branch = true,
+        leader_key = ";",
+        buffer_leader_key = "m",
+    },
+}
