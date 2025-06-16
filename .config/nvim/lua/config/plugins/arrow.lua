de497322ad09a599cd50b099f27fd7cff79f459c
pl3lee <billy.lee@coalitioninc.com>
2025-06-16 16:35:44 -0400

    Accidentally commeted out buffer leader key

--- .config/nvim/lua/config/plugins/arrow.lua
+++ .config/nvim/lua/config/plugins/arrow.lua
@@ -4,6 +4,7 @@ return {
         show_icons = true,
         separate_by_branch = true,
         leader_key = ";",
-        index_keys = "afghjklzxcbnmwrtyuiopAFGHJKLZXVBNM,WRTYUIOP", -- keys mapped to bookmark index, i.e. 1st bookmark will be accessible by 1, and 12th - by c       buffer_leader_key = "m",
+        index_keys = "afghjklzxcbnmwrtyuiopAFGHJKLZXVBNM,WRTYUIOP", -- keys mapped to bookmark index, i.e. 1st bookmark will be accessible by 1, and 12th - by c
+        buffer_leader_key = "m",
     },
 }
