6bacd655a4b44bec54cae0d8da19e36581298a2e
pl3lee <billy.lee@coalitioninc.com>
2025-08-27 17:23:31 -0400

    diffview command

--- .config/nvim/lua/config/plugins/diffview.lua
+++ .config/nvim/lua/config/plugins/diffview.lua
@@ -29,6 +29,16 @@ return {
                 local res = vim.system({ "git", "rev-parse", "--verify", "main" }, { capture_output = true }):wait()
                 return res.code == 0 and "main" or "master"
             end
+
+            local function get_base_commit()
+                local default_branch = get_default_branch_name()
+                local res = vim.system({ "git", "merge-base", "HEAD", default_branch }, { capture_output = true }):wait()
+                if res.code == 0 then
+                    return vim.trim(res.stdout)
+                else
+                    return nil
+                end
+            end
             vim.keymap.set("n", "<leader>gdd", function()
                 vim.cmd("DiffviewOpen")
             end, { desc = "Diff against HEAD" })
@@ -41,6 +51,14 @@ return {
             vim.keymap.set("n", "<leader>gh", function()
                 vim.cmd("DiffviewFileHistory %")
             end)
+            vim.keymap.set("n", "<leader>gdb", function()
+                local base = get_base_commit()
+                if base then
+                    vim.cmd("DiffviewOpen " .. base .. "..HEAD")
+                else
+                    vim.notify("Could not find base commit", vim.log.levels.ERROR)
+                end
+            end, { desc = "Diff against base commit" })
         end,
     },
 }
